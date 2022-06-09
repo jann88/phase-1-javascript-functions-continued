c71b2c34a0a6a0b9a4a8a2cb16d6078ef85fa11b
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-06-09 15:52:20 +0300

    closed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+// Code your solution in this file
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+let wrapAdjective = function(style = "*") {
+    return function(adjective = "special") {
+        return `You are ${style}${adjective}${style}!`
+    }
+}
+
+
+
+
 // code your solution here
